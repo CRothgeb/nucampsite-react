f15d238e20daff288534296ec4cf5c05aa7e5b24
Chris Rothgeb <93162158+CRothgeb@users.noreply.github.com>
2022-03-24 12:07:41 -0400

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -36,7 +36,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -118,7 +118,7 @@ function RenderCampsite({ campsite }) {
     );
 };
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
     if (comments) {
         const dateFormat = { year: 'numeric', month: 'short', day: '2-digit' };
         return (
@@ -132,7 +132,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
                         </p>
                     </div>
                 )}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div >
         );
     }
@@ -177,7 +177,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                 </div>
